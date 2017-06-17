07154a23a4dea72d9ab314c68212469080d20978
MoyMoy18 <jahzeelpadillacoli@students.berkeley.net>
2017-06-17 22:34:07 +0000

    everthing for today

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,16 @@
 class TodoController < ApplicationController 
-def index 
-end
+    def index 
+    end
+    def show
+        if params[:id].to_i == 1
+            @todo_name = "Complete Math Homework"
+            @todo_description = "Problems 7-13"
+        elsif params[:id].to_i == 2
+            @todo_name = "Complete History Homework"
+            @todo_description = "Watch Swing-Kids"
+        elsif params[:id].to_i == 3
+            @todo_name = "Complete Mandarin Homework"
+            @todo_description = "Study for finals"
+        end
+    end
 end
