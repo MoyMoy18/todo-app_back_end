0501bc0f4de9c696f57496c947766f786ffea139
MoyMoy18 <jahzeelpadillacoli@students.berkeley.net>
2017-06-18 17:44:02 +0000

    linked todos with their pages

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -11,6 +11,26 @@ class TodoController < ApplicationController
         elsif params[:id].to_i == 3
             @todo_name = "Complete Mandarin Homework"
             @todo_description = "Study for finals"
+        elsif params[:id].to_i == 4
+            @todo_name = "Complete English Homework"
+            @todo_description = "Read 1984"
+        elsif params[:id].to_i ==5 
+            @todo_name = "Complete Digital Photography Homework"
+            @todo_description = "Take Landscape Photographs"
+        elsif params[:id].to_i ==6 
+            @todo_name = "Complete Japanese Homework"
+            @todo_description = "Practice Kanji"
+        elsif params[:id].to_i ==7 
+            @todo_name = "Complete Chem Homework"
+            @todo_description = "Work on Mars Project"
+        elsif params[:id].to_i ==8 
+            @todo_name = "Complete Life Homework"
+            @todo_description = "Find the Meaning of Life"
+        elsif params[:id].to_i ==9 
+            @todo_name = "Catch up on Reading"
+            @todo_description = "Read a Crap ton of Books"
+        else
+            @todo_name = "This todo does not exist"
         end
     end
 end
